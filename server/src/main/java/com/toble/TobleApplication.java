234a2769874961de3070a56ebcf3f93aeb7534af
Jungma <mbjun3630@naver.com>
2022-09-07 22:08:15 +0900

    feat: server initial commit

---
+++ server/src/main/java/com/toble/TobleApplication.java
@@ -0,0 +1,12 @@
+package com.toble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobleApplication.class, args);
+    }
+}
---
+++ server/src/test/java/com/toble/TobleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
